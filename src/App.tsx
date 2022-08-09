c43801c767afdd62853560e5fe0c0ad89a447836
tiagoliveira555 <tiagoliveira555@gmail.com>
2022-08-09 18:01:00 -0300

    Ajustado responsividade

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import { Header } from "./components/Header";
 import { Info } from "./components/Info";
 import { Task } from "./components/Task";
 
+import styles from "./App.module.css";
+
 const App = () => {
   return (
     <>
       <Header />
-      <Form />
-      <Info />
-      <Task />
+      <div className={styles.container}>
+        <Form />
+        <Info />
+        <Task />
+      </div>
     </>
   );
 };
